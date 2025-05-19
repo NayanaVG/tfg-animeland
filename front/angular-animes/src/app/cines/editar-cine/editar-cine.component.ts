03e40ff3f10472e7610e22646949b9800b39caf1
Ana VG <anagv.94@gmail.com>
2025-05-19 11:54:46 +0200

    feat: (front) función crear cines + CRUD cines

--- front/angular-animes/src/app/cines/editar-cine/editar-cine.component.ts
+++ front/angular-animes/src/app/cines/editar-cine/editar-cine.component.ts
@@ -1,20 +1,22 @@
 import { Component, Input, numberAttribute } from '@angular/core';
 import { CineCreacionDTO, CineDTO } from '../cines';
 import { FormularioCinesComponent } from "../formulario-cines/formulario-cines.component";
+import { SERVICIO_CRUD_TOKEN } from '../../compartidos/proveedores/proveedores';
+import { CinesService } from '../cines.service';
+import { CrearEntidadComponent } from "../../compartidos/componentes/crear-entidad/crear-entidad.component";
+import { EditarEntidadComponent } from "../../compartidos/componentes/editar-entidad/editar-entidad.component";
 
 @Component({
   selector: 'app-editar-cine',
-  imports: [FormularioCinesComponent],
+  imports: [FormularioCinesComponent, CrearEntidadComponent, EditarEntidadComponent],
   templateUrl: './editar-cine.component.html',
-  styleUrl: './editar-cine.component.css'
+  styleUrl: './editar-cine.component.css',
+  providers: [
+    {provide: SERVICIO_CRUD_TOKEN, useClass: CinesService}
+  ]
 })
 export class EditarCineComponent {
   @Input({transform: numberAttribute})
   id!: number;
-
-  cine: CineDTO = {id: 1, nombre: 'Acrópolis', latitud: 42.356811998997884, longitud: -3.6848047899552414}
-
-  guardarCambios(cine: CineCreacionDTO){
-    console.log('editar cine', cine);
-  }
+  formularioCines = FormularioCinesComponent;  
 }
